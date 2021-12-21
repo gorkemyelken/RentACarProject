570ed67bdab6c68b62dba91e3e216077f229703a
Görkem <gorkemyelken@gmail.com>
2021-12-21 17:29:54 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,30 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            CarManager carManager = new CarManager(new EfCarDal());
+
+
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.DailyPrice);
+            }
+           
+
+            
+
+            
+
+        }     
+    }
+}
