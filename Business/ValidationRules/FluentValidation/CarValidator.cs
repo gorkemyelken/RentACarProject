800e22bbee211c950f8c29e2214b211da88127da
Görkem <gorkem_yelken@hotmail.com>
2022-02-25 18:36:53 +0300

    Updated

--- Business/ValidationRules/FluentValidation/CarValidator.cs
+++ Business/ValidationRules/FluentValidation/CarValidator.cs
@@ -12,6 +12,9 @@ namespace Business.ValidationRules.FluentValidation
     {
         public CarValidator()
         {
+            RuleFor(c=> c.DailyPrice).NotEmpty();
+            RuleFor(c=> c.ModelYear).NotEmpty();
+            RuleFor(c=>c.Description).NotEmpty();
             RuleFor(c => c.Description).MinimumLength(2);
             RuleFor(c => c.DailyPrice).GreaterThan(0);
         }
--- Business/ValidationRules/FluentValidation/UserValidator.cs
+++ Business/ValidationRules/FluentValidation/UserValidator.cs
@@ -12,6 +12,10 @@ namespace Business.ValidationRules.FluentValidation
     {
         public UserValidator()
         {
+            RuleFor(u=>u.FirstName).NotEmpty();
+            RuleFor(u=>u.LastName).NotEmpty();
+            RuleFor(u=>u.Password).NotEmpty();
+            RuleFor(u=>u.Email).NotEmpty();
             RuleFor(u => u.FirstName).MinimumLength(2);
             RuleFor(u => u.LastName).MinimumLength(2);
             RuleFor(u => u.Email).MinimumLength(2);
